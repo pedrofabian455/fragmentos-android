764c4363d26769073234ddb2628cad33be0b654e
pedrofabian455 <pedrofabianzarate@gmail.com>
2023-09-13 20:15:35 -0400

    Creacion de layout

---
+++ app/src/main/java/com/pedro/fragmentos/MainActivity.java
@@ -0,0 +1,41 @@
+package com.pedro.fragmentos;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button primerFragmento, segundoFragmento;
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+        primerFragmento = (Button) findViewById(R.id.btnPrimerFragmento);
+        segundoFragmento = (Button) findViewById(R.id.btnSegundoFragmento);
+
+        primerFragmento.setOnClickListener(new View.onClickListener(){
+            @Override
+            public void onClick(View v){
+                cargarFragmento(new PrimerFragmento());
+            }
+        });
+
+        segundoFragmento.setOnClickListener(new View.onClickListener(){
+            @Override
+            public void onClick(View v){
+                cargarFragmento(new SegundoFragmento());
+            }
+        });
+    }
+    private void cargarFragmento(Fragment fragmento){
+FragmentManager manager = this.getSupportFragmentManager();
+manager.beginTransaction().replace(R.id.frameLayout, fragmento).commit();
+
+    }
+
+}
